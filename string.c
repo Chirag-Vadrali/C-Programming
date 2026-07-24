0d18fa32f6d0276f94e42d01e4a73d2a36bc5d8c
Ryujino18 <chiragsv18@gmail.com>
2026-07-25 00:36:38 +0530

    Edited the spaces

--- string.c
+++ string.c
@@ -12,15 +12,19 @@ int main(){
     printf("Enter an adjective1: ");
     fgets(adjective1,sizeof(adjective1),stdin); 
     adjective1[strlen(adjective1)-1] = '\0';
+
     printf("Enter an noun: ");
     fgets(noun,sizeof(noun),stdin); 
     noun[strlen(noun)-1] = '\0';
+    
     printf("Enter an verb: ");
     verb[strlen(verb)-1] = '\0';
     fgets(verb,sizeof(verb),stdin); 
+
     printf("Enter an adjective2: ");
     fgets(adjective2,sizeof(adjective2),stdin);
     adjective2[strlen(adjective2)-1] = '\0';
+    
     printf("Enter an adjective3: ");
     fgets(adjective3,sizeof(adjective3),stdin);
     adjective3[strlen(adjective3)-1] = '\0';
